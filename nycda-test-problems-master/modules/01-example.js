59b5221edc81a1da776ca9545f86a810ce67c186
Justin Loffredo <justloffredo@gmail.com>
2017-05-07 14:35:18 -0400

    Re-organized

--- nycda-test-problems-master/modules/01-example.js
+++
@@ -1,8 +0,0 @@
-// We're going to try fixing the very first test. What you'll need to do is
-// export a variable that has the value "hello". Simply assign a variable to
-// "hello", module.exports the variable, and re-run the test!
-
-let Variable = "hello";
-
-
-module.exports = Variable;
