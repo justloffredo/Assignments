73ac195f4100e37b4fb86352feb2fbc982d5866a
Justin Loffredo <justloffredo@gmail.com>
2017-05-09 15:00:11 -0400

    People Merger File IO exercise

---
+++ people_merger_assignment/people_merger_file.js
@@ -0,0 +1,28 @@
+const fs  = require('fs');
+
+const people1 = require('./people1.json');
+const people2 = require('./people2.json');
+
+let output = "";
+
+let peopleMerged = people1.concat(people2);
+
+for (var i = 0; i < peopleMerged.length; i++) {
+	output += peopleMerged[i] + "\n";
+}
+
+console.log(output);
+
+
+
+
+fs.writeFile('./sorted.people.txt', 'output' , 'utf8', (err) => {
+	if (err)
+		throw (err);
+	console.log("Success");});
+
+//
+// people1.concat(people2)
+// .map(item => item.toLowerCase())
+// .sort()
+// .join('\n');
